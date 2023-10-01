e7f2be18ae2609e6eae1d6d8ced50af1aba72273
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 07:25:08 +0200

    Update pie_chart.py

--- pages/pie_chart.py
+++ pages/pie_chart.py
@@ -13,28 +13,28 @@ tab1, tab2 = st.tabs(["Altair", "Plotly"])
 
 with tab1:
    # Vega-Altair
-import altair as alt
-import pandas as pd
+    import altair as alt
+    import pandas as pd
 
 # Create a sample dataframe
-data = pd.DataFrame({
-  'Category': ['A', 'B', 'C', 'D'],
-  'Value': [10, 15, 7, 10]
-})
+    data = pd.DataFrame({
+      'Category': ['A', 'B', 'C', 'D'],
+      'Value': [10, 15, 7, 10]
+    })
 
 # Create a pie chart
-chart = alt.Chart(data).mark_arc().encode(
-    alt.Theta('Value:Q', stack=True),
-    alt.Color('Category:N')
-).properties(
-    width=400,
-    height=400,
-    title='My Pie Chart'  # Add your title here
-).project(
-    'identity'
-)
+    chart = alt.Chart(data).mark_arc().encode(
+        alt.Theta('Value:Q', stack=True),
+        alt.Color('Category:N')
+    ).properties(
+        width=400,
+        height=400,
+        title='My Pie Chart'  # Add your title here
+    ).project(
+        'identity'
+    )
 
-st.altair_chart(chart)
+    st.altair_chart(chart)
 
    with st.expander("Code"):
     st.write("""
