2118cde69957e1df4bb3cfaec62e2f60f3d20e25
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 06:29:58 +0200

    Update cutecharts.py

--- pages/cutecharts.py
+++ pages/cutecharts.py
@@ -23,9 +23,36 @@ chart = alt.Chart(data).mark_arc().encode(
 
 st.altair_chart(chart)
 
-with st.expander("See explanation"):
+with st.expander("Code"):
     st.write("""
-        The chart above shows some numbers I picked for you.
-        I rolled actual dice for these, so they're *guaranteed* to
-        be random.
+
+    # import necessary libraries
+    
+    import streamlit as st
+    import altair as alt
+    import pandas as pd
+
+    # Create a sample dataframe
+    
+    data = pd.DataFrame({
+    'Category': ['A', 'B', 'C', 'D'],
+    'Value': [10, 15, 7, 10]
+    })
+
+    # Create a pie chart
+    
+    chart = alt.Chart(data).mark_arc().encode(
+        alt.Theta('Value:Q', stack=True),
+        alt.Color('Category:N')
+    ).properties(
+        width=400,
+        height=400,
+        title='My Pie Chart'  # Add your title here
+    ).project(
+        'identity'
+    )
+    
+    # Display Pie Chart
+    
+    st.altair_chart(chart)
     """)
