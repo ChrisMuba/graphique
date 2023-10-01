8f5313db4cd3ddf6ec0158a8822ad04ab5d187da
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 17:47:54 +0200

    Update 3_H_bar_chart.py

--- pages/3_H_bar_chart.py
+++ pages/3_H_bar_chart.py
@@ -80,79 +80,27 @@ with tab3:
    
 # Expanders
 with st.expander("Code Vega-Altair"):
-    st.write("""
-
-    :blue[*import necessary libraries*]
-    
-    import streamlit as st
-    import altair as alt
-    import pandas as pd
-
-    :blue[*Create a sample dataframe*]
-    
-    data = pd.DataFrame({
-    'Category': ['A', 'B', 'C', 'D'],
-    'Value': [10, 15, 7, 10]
-    })
-
-    :blue[*Create a pie chart*]
-    
-    altair_chart = alt.Chart(data).mark_arc().encode(
-        alt.Theta('Value:Q', stack=True),
-        alt.Color('Category:N')
-    ).properties(
-        width=400,
-        height=400,
-        title='My Pie Chart'  # Add your title here
-    ).project(
-        'identity'
-    )
-    
-    :blue[*Display Pie Chart*]
-    
-    st.altair_chart(altair_chart)
-    """)
+   
 
 with st.expander("Code *Plotly*"):
-    st.write("""
-
-    :blue[*import necessary libraries*]
     
-    import plotly.express as px
-
-    :blue[*Create a sample dataframe*]
-    
-    data = {
-    'Category': ['A', 'B', 'C', 'D'],
-    'Value': [10, 15, 7, 10]
-    }
-
-    :blue[*Create a pie chart*]
-    
-    plotly_chart = px.pie(data, values='Value', names='Category', title='My Pie Chart')
-
-    :blue[*Display Pie Chart*]
-    
-    st.plotly_chart(plotly_chart)
-    """)
-
 with st.expander("Code *Matplotlib*"):
     # Display the code
     code = '''
 import matplotlib.pyplot as plt
 
 # Create a sample data
-data = {
-    'Category': ['A', 'B', 'C', 'D'],
-    'Value': [10, 15, 7, 10]
-    }
+data = pd.DataFrame({
+  'Category': ['A', 'B', 'C', 'D', 'E'],
+  'Value': [10, 15, 7, 10, 8],
+  'Color': ['red', 'blue', 'green', 'orange', 'purple']
+})
 
 # Create a bar chart with custom colors
 plt.barh(data['Category'], data['Value'], color=data['Color'])
 plt.xlabel('Value')
 plt.ylabel('Category')
 plt.title('My Horizontal Bar Chart')
-Matplotlib Chart
 
 # Display Matplotlib Horizontal Bar Chart
 st.pyplot(plt)
