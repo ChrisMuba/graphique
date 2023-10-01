4ba60c31ae31e74e8360e5ada0fe5bccf757ba89
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 07:06:24 +0200

    Update pie_chart.py

--- pages/pie_chart.py
+++ pages/pie_chart.py
@@ -78,6 +78,39 @@ tab1, tab2 = st.tabs(["Altair", "Plotly"])
 
 with tab1:
    st.altair_chart(chart)
+   with st.expander("Code"):
+    st.write("""
+
+    :blue[*import necessary libraries*]
+    
+    import streamlit as st
+    import altair as alt
+    import pandas as pd
+
+    :blue[*Create a sample dataframe*]
+    
+    data = pd.DataFrame({
+    'Category': ['A', 'B', 'C', 'D'],
+    'Value': [10, 15, 7, 10]
+    })
+
+    :blue[*Create a pie chart*]
+    
+    chart = alt.Chart(data).mark_arc().encode(
+        alt.Theta('Value:Q', stack=True),
+        alt.Color('Category:N')
+    ).properties(
+        width=400,
+        height=400,
+        title='My Pie Chart'  # Add your title here
+    ).project(
+        'identity'
+    )
+    
+    :blue[*Display Pie Chart*]
+    
+    st.altair_chart(chart)
+    """)
 with tab2:
    st.plotly_chart(fig)
    
