3c0dc2398980bcf4e71d97f626ae4fe4dcd55920
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 05:51:44 +0200

    Update cutecharts.py

--- pages/cutecharts.py
+++ pages/cutecharts.py
@@ -2,18 +2,20 @@
 import streamlit as st
 from cutecharts.charts import Line
 
-
-chart = Line("Line Chart")
-chart.set_options(
-    labels=["A", "B", "C", "D", "E", "F", "G"], 
-    x_label="I'm xlabel", 
-    y_label="I'm ylabel",
-)
-chart.add_series("series-A", [57, 134, 137, 129, 145, 60, 49])
-chart.add_series("series-B", [114, 55, 27, 101, 125, 27, 105])
+def line_chart():
+    chart = Line("Line Chart")
+    chart.set_options(
+        labels=["A", "B", "C", "D", "E", "F", "G"], 
+        x_label="I'm xlabel", 
+        y_label="I'm ylabel",
+    )
+    chart.add_series("series-A", [57, 134, 137, 129, 145, 60, 49])
+    chart.add_series("series-B", [114, 55, 27, 101, 125, 27, 105])
     return chart
 
+def main():
+    st.title("Cutecharts Line Chart with Streamlit")
+    st.write(line_chart().render_notebook())
 
-st.title("Cutecharts Line Chart with Streamlit")
-st.write(line_chart().render_notebook())
-
+if __name__ == "__main__":
+    main()
