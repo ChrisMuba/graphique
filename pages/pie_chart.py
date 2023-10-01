1949ed2b1fd88a3207b5b3e23894b7be8fc35b89
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 07:41:11 +0200

    Update pie_chart.py

--- pages/pie_chart.py
+++ pages/pie_chart.py
@@ -1,43 +1,57 @@
 
 import streamlit as st
 
+# Vega-Altair
+import altair as alt
+import pandas as pd
+
+# Create a sample dataframe
+data = pd.DataFrame({
+   'Category': ['A', 'B', 'C', 'D'],
+   'Value': [10, 15, 7, 10]
+})
+
+# Create a pie chart
+chart = alt.Chart(data).mark_arc().encode(
+   alt.Theta('Value:Q', stack=True),
+   alt.Color('Category:N')
+).properties(
+   width=400,
+   height=400,
+   title='My Pie Chart'  # Add your title here
+).project(
+   'identity'
+)
 
 
 
+   #Plotly
+import plotly.express as px
+
+# Create a sample dataframe
+data = {
+  'Category': ['A', 'B', 'C', 'D', 'E'],
+  'Value': [10, 15, 7, 10, 8]
+}
+
+# Create a pie chart
+fig = px.pie(data, values='Value', names='Category', title='My Pie Chart')
 
 
-
-import streamlit as st
 
 tab1, tab2 = st.tabs(["Altair", "Plotly"])
 
 with tab1:
-   # Vega-Altair
-    import altair as alt
-    import pandas as pd
+   #Display Vega-Altair Chart
+   st.altair_chart(chart) 
+   #Display Plotly Chart
+with tab2:
+   st.plotly_chart(fig)
+   
 
-# Create a sample dataframe
-    data = pd.DataFrame({
-      'Category': ['A', 'B', 'C', 'D'],
-      'Value': [10, 15, 7, 10]
-    })
 
-# Create a pie chart
-    chart = alt.Chart(data).mark_arc().encode(
-        alt.Theta('Value:Q', stack=True),
-        alt.Color('Category:N')
-    ).properties(
-        width=400,
-        height=400,
-        title='My Pie Chart'  # Add your title here
-    ).project(
-        'identity'
-    )
-
-    st.altair_chart(chart)
-
-    with st.expander("Code"):
-    st.write("""
+with st.expander("Code"):
+st.write("""
 
     :blue[*import necessary libraries*]
     
@@ -69,21 +83,8 @@ with tab1:
     
     st.altair_chart(chart)
     """)
-with tab2:
-   #Plotly
-import plotly.express as px
 
-# Create a sample dataframe
-data = {
-  'Category': ['A', 'B', 'C', 'D', 'E'],
-  'Value': [10, 15, 7, 10, 8]
-}
-
-# Create a pie chart
-fig = px.pie(data, values='Value', names='Category', title='My Pie Chart')
-
-st.plotly_chart(fig)
-   with st.expander("Code"):
+with st.expander("Code"):
     st.write("""
 
     :blue[*import necessary libraries*]
@@ -101,8 +102,6 @@ st.plotly_chart(fig)
     
     fig = px.pie(data, values='Value', names='Category', title='My Pie Chart')
 
-
-    
     :blue[*Display Pie Chart*]
     
     st.plotly_chart(fig)
