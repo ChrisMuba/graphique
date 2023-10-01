21b426cde92998446f8130511684a9e5df094b9b
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 07:51:44 +0200

    Update pie_chart.py

--- pages/pie_chart.py
+++ pages/pie_chart.py
@@ -23,9 +23,7 @@ chart = alt.Chart(data).mark_arc().encode(
    'identity'
 )
 
-
-
-   #Plotly
+   # Plotly
 import plotly.express as px
 
 # Create a sample dataframe
@@ -36,6 +34,37 @@ data = {
 
 # Create a pie chart
 fig = px.pie(data, values='Value', names='Category', title='My Pie Chart')
+
+# Bokeh
+from bokeh.plotting import figure
+from bokeh.transform import cumsum
+from bokeh.palettes import Category20c
+from bokeh.models import ColumnDataSource
+import pandas as pd
+
+# Create a sample dataframe
+data = pd.DataFrame({
+  'Category': ['A', 'B', 'C', 'D', 'E'],
+  'Value': [10, 15, 7, 10, 8]
+})
+
+data['angle'] = data['Value']/data['Value'].sum() * 2*pi
+data['color'] = Category20c[len(data)]
+
+p = figure(plot_height=350, title="Pie Chart", toolbar_location=None,
+           tools="hover", tooltips="@Category: @Value", x_range=(-0.5, 1.0))
+
+p.wedge(x=0, y=1, radius=0.4,
+        start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+        line_color="white", fill_color='color', legend_field='Category', source=data)
+
+p.axis.axis_label=None
+p.axis.visible=False
+p.grid.grid_line_color = None
+
+st.bokeh_chart(p)
+
+
 
 # Onglets "Altair", "Plotly" &
 tab1, tab2 = st.tabs(["Altair", "Plotly"])
