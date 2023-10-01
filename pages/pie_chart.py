b4fc40cea0308deeb419e949c815154fbfc60112
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 06:39:51 +0200

    Update pie_chart.py

--- pages/pie_chart.py
+++ pages/pie_chart.py
@@ -32,14 +32,14 @@ with st.expander("Code"):
     import altair as alt
     import pandas as pd
 
-    *Create a sample dataframe*
+    :blue[*Create a sample dataframe*]
     
     data = pd.DataFrame({
     'Category': ['A', 'B', 'C', 'D'],
     'Value': [10, 15, 7, 10]
     })
 
-    *Create a pie chart*
+    :blue[*Create a pie chart*]
     
     chart = alt.Chart(data).mark_arc().encode(
         alt.Theta('Value:Q', stack=True),
@@ -52,7 +52,7 @@ with st.expander("Code"):
         'identity'
     )
     
-    *Display Pie Chart*
+    :blue[*Display Pie Chart*]
     
     st.altair_chart(chart)
     """)
