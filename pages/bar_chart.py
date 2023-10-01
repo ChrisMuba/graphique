8f90b32dfaf8545403c021880cb0b73ed61eb433
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 08:28:38 +0200

    Update bar_chart.py

--- pages/bar_chart.py
+++ pages/bar_chart.py
@@ -2,26 +2,28 @@
 import streamlit as st
 
 # Vega-Altair
+import streamlit as st
 import altair as alt
 import pandas as pd
 
 # Create a sample dataframe
 data = pd.DataFrame({
-   'Category': ['A', 'B', 'C', 'D'],
-   'Value': [10, 15, 7, 10]
+  'Category': ['A', 'B', 'C', 'D', 'E'],
+  'Value': [10, 15, 7, 10, 8]
 })
 
-# Create a pie chart
-altair_chart = alt.Chart(data).mark_arc().encode(
-   alt.Theta('Value:Q', stack=True),
-   alt.Color('Category:N')
+# Create a bar chart
+altair_chart = alt.Chart(data).mark_bar().encode(
+    x='Category',
+    y='Value'
 ).properties(
-   width=400,
-   height=400,
-   title='My Pie Chart'  # Add your title here
-).project(
-   'identity'
+    width=400,
+    height=400,
+    title='My Bar Chart'
 )
+
+st.altair_chart(altair_chart)
+
 
    # Plotly
 import plotly.express as px
