97c3b28d20a6fd0eef26833db3298e38fe4513ae
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 05:49:06 +0200

    Update cutecharts.py

--- pages/cutecharts.py
+++ pages/cutecharts.py
@@ -1,15 +1,19 @@
 
 import streamlit as st
-from cutecharts.charts import Bar
-from cutecharts.components import Page
-from cutecharts.faker import Faker
+from cutecharts.charts import Line
 
-def bar_chart():
-    chart = Bar("My Cute Bar Chart")
-    chart.set_options(labels=Faker.choose(), x_label="X", y_label="Y")
-    chart.add_series("series-A", Faker.values())
+
+    chart = Line("Line Chart")
+    chart.set_options(
+        labels=["A", "B", "C", "D", "E", "F", "G"], 
+        x_label="I'm xlabel", 
+        y_label="I'm ylabel",
+    )
+    chart.add_series("series-A", [57, 134, 137, 129, 145, 60, 49])
+    chart.add_series("series-B", [114, 55, 27, 101, 125, 27, 105])
     return chart
 
 
-st.title("Cutecharts Bar Chart with Streamlit")
-st.write(bar_chart().render_notebook())
+    st.title("Cutecharts Line Chart with Streamlit")
+    st.write(line_chart().render_notebook())
+
