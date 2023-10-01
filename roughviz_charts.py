9d654bf59c98e3c4731745671cddc00295213c3e
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 03:59:32 +0200

    Update roughviz_charts.py

--- roughviz_charts.py
+++ roughviz_charts.py
@@ -1,1 +1,10 @@
 
+
+import roughviz
+
+# Bar Chart
+import pandas as pd 
+d = {'Year': ['1980', '1981', '1982'], 'A': [3, 4, 10]}
+df = pd.DataFrame(data=d)
+roughviz.bar(df["Year"], df["A"], axisRoughness = 0.7, axisStrokeWidth = 0.7, roughness=2.3, highlight="gray")
+
