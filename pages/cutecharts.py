d66f1395685afd713eda8e8876391ba3fc891108
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-01 04:49:34 +0200

    Update cutecharts.py

--- pages/cutecharts.py
+++ pages/cutecharts.py
@@ -4,7 +4,7 @@ import cutecharts.charts as ctc
 
 # import data
 df=pd.DataFrame({
- ‘x’:[‘Sun.’,’Mon.’,’Tue.’,’Wed.’,’Thu.’,’Fri.’,’Sat.’],
+  x:[‘Sun.’,’Mon.’,’Tue.’,’Wed.’,’Thu.’,’Fri.’,’Sat.’],
  ‘y’:[14,15,17,20,22.3,23.7,24.8],
  ‘z’:[16,16.4,23.6,24.5,19.9,13.6,13.4]})
 
