f93e238eb40104c3929d076e8cecfe1a00c61664
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-02 22:35:47 +0200

    Update 🏠_home.py

--- 🏠_home.py
+++ 🏠_home.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.title("Bienvenue dans la :blue[galerie graphique de l'analyste de données !] 📊")
 
-st.sidebar.success("Selectionner un graphique.📈")
+st.sidebar.success("Cliquez sur un graphique.📈")
 
 
 st.caption("Vous souhaitez faire analyser vos data, prendre des décisions éclairées ou bénéficier d'un tutorat personnalisé ? parlons‑en : 📧https://www.linkedin.com/in/chris-muba-io 🌍")
